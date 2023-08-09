3caee19869d344dca22efe8f5b431bca5d6d9dbd
ZHANG Xin <sean.zhang@connect.ust.hk>
2023-08-10 00:57:14 +0800

    upd

--- test_installation.py
+++ test_installation.py
@@ -3,14 +3,13 @@ import torch
 print('==================')
 print('test installation')
 g = dgl.rand_graph(1000,10000).formats(['csc'])
-g.pin_memory_()
-assert g.is_pinned()
-rowptr, col, _ = g.adj_sparse(fmt='csc')
-cached_indptr = rowptr[:100].cuda()
-cached_indices = col[:100].cuda()
-seed_nodes = torch.arange(10).cuda()
-fanout = 10
 try:
+    g.pin_memory_()
+    rowptr, col, _ = g.adj_sparse(fmt='csc')
+    cached_indptr = rowptr[:10].cuda()
+    cached_indices = col[:100].cuda()
+    seed_nodes = torch.arange(10).cuda()
+    fanout = 10
     sg = dgl.sampling.sample_neighbors_with_cache(g, cached_indptr, cached_indices, seed_nodes, fanout)
     print('test successful') 
 except Exception as error:
